54e0d855e3fd83fcab83d6c9d2c295b8696f35bd
ks_sha <68646823+KsuKad@users.noreply.github.com>
2021-01-08 03:31:50 +0700

    Add files via upload

--- 5/5.js
+++ 5/5.js
@@ -1,26 +1,13 @@
 let first = document.querySelector("#uniq1");
-let second= document.querySelector("#uniq2");
+let second = document.querySelector("#uniq2");
 let third = document.querySelector("#uniq3");
 
-
-function openForm_1(){
-  first.style.visibility = "visible";
-}
- function openForm_2(){
-  second.style.visibility = "visible";
-}
-function openForm_3(){
-  third.style.visibility = "visible";
+openForm = (id) => {
+  document.getElementById(id).style.visibility = "visible";
 }
 
-
-
-first.onclick = function(){
-  first.style.visibility = "hidden";
-}
-second.onclick = function(){
-  second.style.visibility = "hidden";
-}
-third.onclick = function(){
-  third.style.visibility = "hidden";
-}
+document.querySelectorAll('.modal').forEach((modal) => {
+  modal.addEventListener('click', () => {
+    modal.style.visibility = "hidden";
+  })
+});
